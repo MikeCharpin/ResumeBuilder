e93a3caa1e3089909aef1de43fc3de4635c7c5aa
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2023-11-28 18:15:00 -0500

    Changed file location.

---
+++ src/example-data.tsx
@@ -0,0 +1,9 @@
+export default const exampleData = {
+    personalInfo: {
+        fullName: "Mike Charpin",
+        emial: "Michael.Charpin@gmail.com",
+        location: "Brooklyn, NY",
+        linkedin: "in/michael-charpin",
+        website: "MichaelCharpin.com",
+    }
+}
