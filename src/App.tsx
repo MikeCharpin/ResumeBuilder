2ec7ff940fc8357b6c1a2ff99f12badbc3642beb
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2023-12-01 13:41:18 -0500

    Adding state managment to the personal info.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react"
 import Resume from "./components/Resume"
 import Sidebar from "./components/Sidebar"
+import { exampleData } from "./example-data"
+
+export type PersonalInfoProps = {
+  fullName: string
+}
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState(exampleData.personalInfo)
+  function handlePersonalInfoChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { key } = e.target.dataset as {key: string}
+    setPersonalInfo({ ...personalInfo, [key]: e.target.value })
+  }
+
 
 
   return (
   <div className="flex items-start justify-between bg-slate-400 h-screen gap-4 p-4">
-    <Sidebar />
-    <Resume />
+    <Sidebar
+      onChange={handlePersonalInfoChange}
+      personalInfo={personalInfo}
+    />
+    <Resume
+      personalInfo={personalInfo}
+    />
   </div>
   )
 }
