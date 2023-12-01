d32d4d62550a087de616a71b3f5497bafacd78bc
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2023-12-01 13:40:39 -0500

    Removing custom text sizes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontSize: {
-      xs: '.3rem',
-      sm: '.35rem',
-      base: '.4rem',
-      lg: '.5rem',
-      xl: '.55rem',
-      "2xl": '.6rem'
-    },
+
     extend: {},
   },
   plugins: [],
